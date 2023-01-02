24dae48eb2445c0010a79ea6c79f1863e6dde287
caobandoc <obandocarlosandres@gmail.com>
2023-01-02 14:30:26 -0500

    arreglo paginacion

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
 import { CategoryElement } from 'src/app/models/category-element.model';
@@ -16,6 +17,9 @@ export class CategoryComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
   dataSource = new MatTableDataSource<CategoryElement>();
+  
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
   constructor(
     private categoryService: CategoryService,
@@ -49,6 +53,7 @@ export class CategoryComponent implements OnInit {
       });
 
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+      this.dataSource.paginator = this.paginator;
     }
   }
 
